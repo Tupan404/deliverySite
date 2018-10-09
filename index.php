33d7f0449dff977c4cc5dade71bf927d73891a2a
Eduado Tupan <eduardotupantupan@gmail.com>
2018-10-08 21:25:24 -0400

    Iindex
    Index do site em andamento

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>  
+    <meta charset="UTF-8">
+    <link rel="icon" href="https://png.icons8.com/metro/1600/delivery.png">
+    <link rel="stylesheet" type="estilo/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title> Delivery Master  </title>
+
+    <style>
+        .barra{
+    background-color: #00a2d5;
+    position: fidex;
+    top: -60px;
+    left: auto;
+    right: auto ;
+    height: 30px;
+}
+    img.usuario{
+        position:absolute;
+    margin-top: auto;
+    margin-left:auto;
+    margin-right:auto;
+   
+
+}
+
+    img.logo{
+        position:absolute;
+    margin-top: -250px;
+    margin-left:550px;
+    margin-right:auto;
+    }
+
+    .carrinho{
+        position:absolute;
+    margin-top: -160px; 
+    margin-left:1100px;
+    margin-righ t:0;
+    }
+
+     .icon{
+        position:absolute;
+    margin-top: -120px;
+    margin-left:1120px;
+    margin-right:0;
+    
+    }
+
+
+    </style>
+
+</head>
+<body>
+
+
+<div class="container-fluid">   
+    <!-- <div class="row">
+        <div class="col-12"> -->
+
+
+    <div class="barra">
+    <a href="usuario.php"> <img src="https://openclipart.org/download/247319/abstract-user-flat-3.svg" class="float-right" width="20" height="20" > </a>
+    </div>
+
+  <!-- </div>
+</div> -->
+</div>
+    
+   <div class="container-fluid" style="background-color:#fff;">
+                        <div class="row">
+                            <div class="col-12"> 
+                                
+                                        
+                                 <!-- <div class="row"> -->
+                                     
+
+                                        <div class="col-10">
+                                        
+                                         <img src="alimento2.jpg" class="img-responsive" width="1300" height="300" alt="Responsive image">
+                                           <img src="logo.png"  class="logo" alt="Responsive image">
+                                            
+                                           <button type="button" class="carrinho btn badge badge-pill badge-light">Meu Carrinho  </button>
+                                           <img src="http://www.youthhorizons.org/wp-content/uploads/2017/02/cart-138x150.png" class="icon" width="80" height="80">
+
+                                         </div>
+                                      
+                                <!-- </div> -->
+                             
+                            </div>
+                        </div> 
+                      </div>
+
+    <div class="container-fluid" style="background-color:#00a2d5;">
+    <br> 
+       <div class="row">
+            <div class="col-10 offset-1">
+
+                <div class="container-fluid" style="background-color:#fff;">
+                 <h4 class="text-center text-dark "> Escolha um mercado para realizar suas compras </h4>
+
+                    <div class="row">
+                        <div class="col-2">              
+                        
+                        <a href="http://www.irmaosgoncalves.com.br/" target="_blanck"><img class="border border-info rounded-circle" src="http://www.unimake.com.br/images/parceiros/danfeviewoffice/irmaosgoncalves.png" width="150" height="150"></a>
+                        </div>
+
+                        <div class="col-2">
+                        <a href="https://www.atacadao.com.br/" target="_blanck"><img class="border border-info rounded-circle" src="http://fingersites.com.br/cozil-final/283-tm_large_default/atacadao.jpg" width="150" height="150"></a>
+                        </div>
+                    
+                        </div> <br>
+                </div>
+                        
+             </div>
+        </div>  <br>       
+    </div>
+
+    <div class="container-fluid" style="background-color:#00a2d5;">
+    <br> 
+       <div class="row">
+            <div class="col-5 offset-1" style="background-color:#fff;">
+
+                <div class="container-fluid"> <br>
+                 <h3 class="text-center text-secondary"> Sobre Nosso Software </h3>
+                 <p class="lead text-justify"> 
+                 Delivery Master sempre esteve e sempre estará aqui para atender à comunidade. 
+                 Acreditamos que é importante oferecer aos nossos clientes produtos de qualidade e um ótimo serviço. 
+                 Desde o início o que define quem somos e o que fazemos é nossa verdadeira paixão pela clientela. 
+                 Navegue pelo site e entre em contato para mais informações sobre o(a) Delivery Master.
+                 </p>
+                <br>
+                </div> 
+                        
+            </div>
+
+            <div class="col-5"  style="background-color:#fff;">
+                <img src="team.png" width="400" heigth="400">
+                <br>
+            </div>
+
+        </div>     <br>      
+    </div>
+
+    <div class="container-fluid" style="background-color:#00a2d5;">
+    <br> 
+       <div class="row">
+            <div class="col-10 offset-1" style="background-color:#fff;">
+            <div class="shadow-lg p-3 mb-5 bg-white rounded">
+            <form>
+                <div class="form-row">
+                    <div class="col-md-4 mb-3">
+                    <label for="validationDefault01">First name</label>
+                    <input style="border:none;border-bottom: 2px solid black;" type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Mark" required>
+                    </div>
+    
+            </form>
+            
+            </div>
+            </div>   
+                        
+        </div>
+
+                      
+    </div>
+
+</body>
+</html>
