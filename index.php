7620660756259ad9c3ef99f5d42feabcc2a0593d
Eduado Tupan <eduardotupantupan@gmail.com>
2018-10-09 00:54:42 -0400

    Site do projeto Delivery
    Site em construção ainda

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     top: -60px;
     left: auto;
     right: auto ;
-    height: 30px;
+    height: 35px;
 }
     img.usuario{
         position:absolute;
@@ -36,7 +36,7 @@
         position:absolute;
     margin-top: -160px; 
     margin-left:1100px;
-    margin-righ t:0;
+    margin-right:0;
     }
 
      .icon{
@@ -53,6 +53,7 @@
 </head>
 <body>
 
+            <!-- CONTEUDO INICIAL -->
 
 <div class="container-fluid">   
     <!-- <div class="row">
@@ -60,7 +61,7 @@
 
 
     <div class="barra">
-    <a href="usuario.php"> <img src="https://openclipart.org/download/247319/abstract-user-flat-3.svg" class="float-right" width="20" height="20" > </a>
+    <a href="usuario.php"> <img src="https://openclipart.org/download/247319/abstract-user-flat-3.svg" class="float-right" width="20" height="20" style="margin-top:10px;margin:10px;" > </a>
     </div>
 
   <!-- </div>
@@ -77,7 +78,7 @@
 
                                         <div class="col-10">
                                         
-                                         <img src="alimento2.jpg" class="img-responsive" width="1300" height="300" alt="Responsive image">
+                                         <img src="alimento2.jpg"  width="1300" height="300" >
                                            <img src="logo.png"  class="logo" alt="Responsive image">
                                             
                                            <button type="button" class="carrinho btn badge badge-pill badge-light">Meu Carrinho  </button>
@@ -90,15 +91,38 @@
                             </div>
                         </div> 
                       </div>
+                      <!-- FECHA CONTEUDO INICAL -->
 
-    <div class="container-fluid" style="background-color:#00a2d5;">
+                      <!-- SAUDAÇÕES DE BEM VINDO AO SITE -->
+     <div class="container-fluid" style="background-color:#333645;">
     <br> 
+        <h4 class="text-center text-info display-3"> Bem-Vindo ao  </h4>
+        <h1 class="text-center text-info display-4"> Delivery Master  </h1> <br>
+        <p class="lead text-center text-white"> A Melhor Experiencia em Compras !!! </p> <br>
+
+        <div class="col-10 offset-1">
+         <blockquote class="blockquote text-center text-white">
+            <p class="mb-0"> Desde nossa inauguração em 2018, temos o orgulho de atender a população de Ji-Paraná.
+                 Através do Delivery Master, nosso compromisso não se restringe a fornecer produtos e serviços de qualidade,
+                 mas sim fazer o máximo para garantir que nossos clientes fiquem completamente satisfeitos.
+                 Convidamos você a navegar pelo site para conhecer tudo o que temos para oferecer. </p>
+         </blockquote>
+        </div>
+    <br>                
+    </div>
+                        <!-- FECHA SAUDAÇÕES DE BEM VINDO -->
+
+                        <!-- PARTE PRINCIPAL DA INDEX - ESCOLHER O MERCADO -->
+    <div class="container-fluid" style="background-color:#fff;">
+    <br> 
+    <h4 class="text-center text-dark display-4"> Escolha um mercado para realizar suas compras </h4> <br>
        <div class="row">
+      
+
             <div class="col-10 offset-1">
-
+           
                 <div class="container-fluid" style="background-color:#fff;">
-                 <h4 class="text-center text-dark "> Escolha um mercado para realizar suas compras </h4>
-
+                 
                     <div class="row">
                         <div class="col-2">              
                         
@@ -115,7 +139,9 @@
              </div>
         </div>  <br>       
     </div>
+                                <!-- FECHA ESCOLHER MERCADO -->
 
+                                <!-- SOBRE NOSSA EQUIPE TOP DO GOIAS -->
     <div class="container-fluid" style="background-color:#00a2d5;">
     <br> 
        <div class="row">
@@ -141,6 +167,76 @@
 
         </div>     <br>      
     </div>
+                                <!-- FECHA TEMA SOBRE EQUIPE -->
+
+                                <!-- SOBRE NOSSOS SERVIÇOS -->
+    <div class="container-fluid" style="background-color:#fff;">
+    <br> 
+    
+       <div class="row">
+      
+
+            <div class="col-10 offset-1">
+           
+                <div class="container-fluid" style="background-color:#fff;">
+                <h2 class="text-center text-secondary"> Nossos Serviços </h2> <br>
+                    <p class="lead text-center"> Conheça mais sobre nossos serviços </p>
+                </div>
+                        
+             </div>
+        </div>  <br>       
+    </div>
+                                <!-- FECHA CONTEUDO SOBRE SERVIÇOS -->
+
+                                <!-- NOSSO SERVIÇO EM DESTAQUE -->
+      <div class="container-fluid" style="background-color:#333645;">
+    <br> 
+    <div class="col-10 offset-1">
+           
+           
+            
+             <div class="row">
+                   <div class="col-6">   
+                      <img src="https://s3.amazonaws.com/igd-wp-uploads/2016/05/Como-Fazer-uma-Pesquisa-de-Mercado-para-o-seu-Nego%CC%81cio-Online.jpg" width="700" heigth="400">         
+                      <!-- https://ecommercenews.com.br/wp-content/uploads/2016/06/carrinho-de-compra.jpg -->
+                    </div>
+
+             
+                   <div class="col-6" style="background-color:#fff;">   
+                        <h3 class="text-center"> Mercado Online </h3> <br>
+                        <p class="lead text-justify"> Este é um dos nossos serviços mais populares. 
+                        Fez uma grande diferença para muitos de nossos clientes, e é oferecido no mais alto nível de excelência.
+                         Com ele, asseguramos que todos os detalhes sejam simples, dinâmicos e executados em tempo útil. 
+                        Sempre que trabalhar com o(a) Delivery Master, você pode confiar que estará em ótimas mãos. <p> 
+                    </div>       
+            </div> <br>
+
+            <div class="row">
+                   <div class="col-6">   
+                      <img src="http://dconheels.com/wp-content/uploads/2018/09/grocery-deivery-shutterstock.jpg" width="700" heigth="400">         
+                    </div>
+
+             
+                   <div class="col-6" style="background-color:#fff;">   
+                        <p class="lead text-center"> Entregas </p>
+                        <p class="text-justify"> Este é um dos nossos serviços mais populares. 
+                        Fez uma grande diferença para muitos de nossos clientes, e é oferecido no mais alto nível de excelência.
+                         Com ele, asseguramos que todos os detalhes sejam simples, dinâmicos e executados em tempo útil. 
+                        Sempre que trabalhar com o(a) Delivery Master, você pode confiar que estará em ótimas mãos. <p> 
+                    </div>       
+            </div>
+
+
+
+
+
+        </div>  <br>       
+    </div>
+                            <!-- FECHA NOSSO SERVIÇO -->
+
+    
+
+                    <!-- FORMULARIO DE CONTATO
 
     <div class="container-fluid" style="background-color:#00a2d5;">
     <br> 
@@ -162,7 +258,7 @@
         </div>
 
                       
-    </div>
+    </div> -->
 
 </body>
 </html>
