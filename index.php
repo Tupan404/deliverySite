6b79d7f4d9bac6973890d31201b4840c10f584b2
Eduado Tupan <eduardotupantupan@gmail.com>
2018-10-11 04:09:00 -0400

    Index
    implementanto view do site

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <link rel="icon" href="https://png.icons8.com/metro/1600/delivery.png">
     <link rel="stylesheet" type="estilo/css" href="style.css">
+    <link href="cover.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title> Delivery Master  </title>
 
@@ -44,8 +45,9 @@
     margin-top: -120px;
     margin-left:1120px;
     margin-right:0;
-    
-    }
+}
+
+}
 
 
     </style>
@@ -56,27 +58,21 @@
             <!-- CONTEUDO INICIAL -->
 
 <div class="container-fluid">   
-    <!-- <div class="row">
-        <div class="col-12"> -->
+    
 
 
     <div class="barra">
     <a href="usuario.php"> <img src="https://openclipart.org/download/247319/abstract-user-flat-3.svg" class="float-right" width="20" height="20" style="margin-top:10px;margin:10px;" > </a>
     </div>
 
-  <!-- </div>
-</div> -->
+
 </div>
     
    <div class="container-fluid" style="background-color:#fff;">
                         <div class="row">
                             <div class="col-12"> 
                                 
-                                        
-                                 <!-- <div class="row"> -->
-                                     
-
-                                        <div class="col-10">
+                            <div class="col-10">
                                         
                                          <img src="alimento2.jpg"  width="1300" height="300" >
                                            <img src="logo.png"  class="logo" alt="Responsive image">
@@ -84,9 +80,7 @@
                                            <button type="button" class="carrinho btn badge badge-pill badge-light">Meu Carrinho  </button>
                                            <img src="http://www.youthhorizons.org/wp-content/uploads/2017/02/cart-138x150.png" class="icon" width="80" height="80">
 
-                                         </div>
-                                      
-                                <!-- </div> -->
+                            </div>                             
                              
                             </div>
                         </div> 
@@ -175,31 +169,29 @@
     
        <div class="row">
       
-
-            <div class="col-10 offset-1">
+        <div class="col-10 offset-1">
            
-                <div class="container-fluid" style="background-color:#fff;">
+                <div class="container-fluid" style="background-color:#fff;"> <br>
                 <h2 class="text-center text-secondary"> Nossos Serviços </h2> <br>
                     <p class="lead text-center"> Conheça mais sobre nossos serviços </p>
                 </div>
-                        
+              <br>            
              </div>
         </div>  <br>       
     </div>
                                 <!-- FECHA CONTEUDO SOBRE SERVIÇOS -->
+
 
                                 <!-- NOSSO SERVIÇO EM DESTAQUE -->
       <div class="container-fluid" style="background-color:#333645;">
     <br> 
     <div class="col-10 offset-1">
            
-           
-            
-             <div class="row">
-                   <div class="col-6">   
+           <div class="row">
+                <div class="col-6">   
                       <img src="https://s3.amazonaws.com/igd-wp-uploads/2016/05/Como-Fazer-uma-Pesquisa-de-Mercado-para-o-seu-Nego%CC%81cio-Online.jpg" width="700" heigth="400">         
                       <!-- https://ecommercenews.com.br/wp-content/uploads/2016/06/carrinho-de-compra.jpg -->
-                    </div>
+            </div>
 
              
                    <div class="col-6" style="background-color:#fff;">   
@@ -218,47 +210,151 @@
 
              
                    <div class="col-6" style="background-color:#fff;">   
-                        <p class="lead text-center"> Entregas </p>
-                        <p class="text-justify"> Este é um dos nossos serviços mais populares. 
+                   <h3 class="text-center"> Entregas </h3> <br>
+                         <p class="lead text-justify"> Este é um dos nossos serviços mais populares. 
                         Fez uma grande diferença para muitos de nossos clientes, e é oferecido no mais alto nível de excelência.
                          Com ele, asseguramos que todos os detalhes sejam simples, dinâmicos e executados em tempo útil. 
                         Sempre que trabalhar com o(a) Delivery Master, você pode confiar que estará em ótimas mãos. <p> 
                     </div>       
             </div>
 
-
-
-
-
         </div>  <br>       
     </div>
                             <!-- FECHA NOSSO SERVIÇO -->
 
-    
 
-                    <!-- FORMULARIO DE CONTATO
+                            <!-- HORARIOS DE ATENDIMENTO -->
+    <div class="container-fluid" style="background-color:#00A2D5;">
+    <br> <br>
+            <div class="text-center">
+            <img src="https://cdn0.iconfinder.com/data/icons/seo-and-internet-marketing-set-2/100/Time_Saving-512.png" width="50" heigth="50">
+            </div> <br>
 
-    <div class="container-fluid" style="background-color:#00a2d5;">
+            <p class="text-center lead"> Horario de Funcionamento </p> <br>
+
+            <p class="text-center lead"> Venha Fazer Sua Compra Online </p> <br>
+            <p class="text-center"> Seg - Sab: 8:00 - 17:00 </p> 
+            <p class="text-center"> Dom: Fechado</p> 
+            <p class="text-center"> 17:00 - 8:00 Pedidos e Entregas em Modo Offline </p> 
+        <br>
+    </div>
+                            <!-- FECHA HORARIOS DE ATENDIMENTO -->
+
+                
+                            <!-- FORMULARIO DE CONTATO -->
+    <div class="container-fluid" style="background-color:#333645;">
     <br> 
        <div class="row">
-            <div class="col-10 offset-1" style="background-color:#fff;">
-            <div class="shadow-lg p-3 mb-5 bg-white rounded">
-            <form>
-                <div class="form-row">
-                    <div class="col-md-4 mb-3">
-                    <label for="validationDefault01">First name</label>
-                    <input style="border:none;border-bottom: 2px solid black;" type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Mark" required>
-                    </div>
-    
-            </form>
-            
-            </div>
-            </div>   
-                        
-        </div>
+        
+            <div class="col-10 offset-1" style="background-color:#fff;"> <br>
+            <h4 class="text-center"> Entre em Contato Conosco </h4> <br>
 
-                      
-    </div> -->
+                <form class="needs-validation" novalidate>
+                    <div class="form-row">
+
+                        <div class="col-md-4 mb-3">
+                        <label for="validationTooltip01">Nome</label>
+                        <input style="border:none;border-bottom: 2px solid black;" type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Nome" required>
+                            <div class="valid-tooltip">
+                                Looks good!
+                            </div>
+                        </div>
+                    
+                    <div class="col-md-4 mb-3">
+                        <label for="validationTooltip01">Telefone</label>
+                        <input style="border:none;border-bottom: 2px solid black;" type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Telefone" required>
+                        <div class="valid-tooltip">
+                              Looks good!
+                        </div>
+                    </div>
+
+                    <div class="col-md-7 mb-3">
+                        <label for="exampleInputEmail1">Email </label>
+                        <input  style="border:none;border-bottom: 2px solid black;" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+                    </div>
+
+                    <div class="col-md-7 mb-3">
+                        <label for="validationTooltip01">Endereço</label>
+                        <input style="border:none;border-bottom: 2px solid black;" type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Endereço" required>
+                        <div class="valid-tooltip">
+                              Looks good!
+                        </div>
+                    </div>
+
+                    <div class="col-md-7 mb-3">
+                        <label for="validationTooltip01">Assunto</label>
+                        <input style="border:none;border-bottom: 2px solid black;" type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Assunto" required>
+                        <div class="valid-tooltip">
+                              Looks good!
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-7 mb-3">
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Mensagem</label>
+                        <textarea style="border:none;border-bottom: 2px solid black;" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    </div>    
+
+                </div> 
+                    <button class="btn btn-info" type="submit">Submit form</button>
+                </form>
+              <br>
+
+               
+
+            </div>   
+            <div class="col-12 text-center">
+                <img src="https://images.e-konomista.pt/articles/850_400_transferencia-de-local-de-trabalho.jpg" width="1125" heith="200">
+            </div>
+        </div>
+    </div>
+
+    <footer class="pt-4 my-md-5 pt-md-5 border-top bg-dark">
+        <div class="row">
+          <div class="col-12 col-md">
+            <img class="mb-2" src="../../assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+            <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Features</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Cool stuff</a></li>
+              <li><a class="text-muted" href="#">Random feature</a></li>
+              <li><a class="text-muted" href="#">Team feature</a></li>
+              <li><a class="text-muted" href="#">Stuff for developers</a></li>
+              <li><a class="text-muted" href="#">Another one</a></li>
+              <li><a class="text-muted" href="#">Last time</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Resources</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Resource</a></li>
+              <li><a class="text-muted" href="#">Resource name</a></li>
+              <li><a class="text-muted" href="#">Another resource</a></li>
+              <li><a class="text-muted" href="#">Final resource</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>About</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Team</a></li>
+              <li><a class="text-muted" href="#">Locations</a></li>
+              <li><a class="text-muted" href="#">Privacy</a></li>
+              <li><a class="text-muted" href="#">Terms</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em"
+        crossorigin="anonymous"></script>
+    <script src="https://igorescobar.github.io/jQuery-Mask-Plugin/js/jquery.mask.min.js"></script>
 
 </body>
 </html>
