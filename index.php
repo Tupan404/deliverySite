a8f0f4afaae54a2f0dd9e4a6b5a56a65f10994c5
Eduado Tupan <eduardotupantupan@gmail.com>
2018-10-11 04:04:27 -0400

    Revert "index"
    This reverts commit 3b4e0830d96979293307792f8784deb4c5b1a484.

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<!DOCTYPE html>
+<html lang="br">
+<head>  
+    <meta charset="UTF-8">
     <link rel="icon" href="https://png.icons8.com/metro/1600/delivery.png">
-    <title>Comprar Produtos</title>
+    <link rel="stylesheet" type="estilo/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title> Delivery Master  </title>
 
-  <style>
+    <style>
         .barra{
     background-color: #00a2d5;
     position: fidex;
     top: -60px;
     left: auto;
     right: auto ;
-    height: 65px;
+    height: 35px;
 }
     img.usuario{
         position:absolute;
@@ -47,146 +44,221 @@
     margin-top: -120px;
     margin-left:1120px;
     margin-right:0;
-}
-
-
-	    .nav{
-	    	padding: 10px 0px;
-	    	background: #37f;
-	    	text-align: center;
-	    }
-
-         .main-menu>li{
-         	display: inline-block;
-         	width: 150px;
-         	padding: 10px 0px;
-         	margin-left: 10px;
-         	text-align: center;
-         	color: white;
-         	font-size: 20px;
-         	border-left: 1px solid white;
-         	transition: .4s;
-         	cursor: pointer;
-         	box-sizing: border-box;
-
-         }
-         
-         .main-menu li:hover .sub-menu{
-              display: block;
-         }
- 
-         .sub-menu{
-         	display: none;
-         	position: absolute;
-         	padding: 0px;
-         	margin-top: 30px;
-         	margin-left: -10px;	
-         	float: left;
-         	width: 300px; 
-         	text-align: left;
-         	box-sizing: border-box;
-         	border: 5px solid white;
-         	box-shadow: 1px 1px 1px grey;
-         	
-       
-         }
-
-         .sub-menu:before{
-                 content:"";
-                 width: 0px;
-                 height:0px;
-                 border-left:20px solid transparent;
-                 border-right:20px solid transparent;
-                 border-bottom:30px solid white;
-                 position: absolute;
-                 margin:-30px 0px 0px 60px;
-         }
-
-         .sub-menu li{
-         	list-style-type: none;
-         	padding: 10px;
-         	color: white;
-         	font-size: 20px;
-         	background: #37f;
-         }
-     .main-menu>li:hover, .sub-menu>li:hover{
-         	background: #35c;
-         }
-
-
-}
+    
+    }
 
 
     </style>
 
-  </head>
-  <body>
-    
-  <div class="container-fluid" style="background-color:#fff;">
-    <div class="row">
-        <div class="col-12">
+</head>
+<body>
 
-            <div class="col-10">
+            <!-- CONTEUDO INICIAL -->
 
-                <img src="alimento2.jpg" width="1300" height="300">
-                <img src="logo.png" class="logo" alt="Responsive image">
-
-                <button type="button" class="carrinho btn badge badge-pill badge-light">Meu Carrinho </button>
-                <img src="http://www.youthhorizons.org/wp-content/uploads/2017/02/cart-138x150.png" class="icon" width="80"
-                    height="80">
-
-            </div>
-
-        </div>
-    </div>
-</div>
-
-<div class="nav">
-		<ul class="main-menu">
-		
-			<li>Course
-
-			   <ul class="sub-menu">
-               	<li>HTML</li>
-               	<li>CSS</li>
-               	<li>JAVASCRIPT</li>
-               	<li>JQUERY</li>
-               	<li>BOOTSTRAP</li>
-               </ul>
-
-            </li>
-  </ul>
-</div>
+<div class="container-fluid">   
+    <!-- <div class="row">
+        <div class="col-12"> -->
 
 
-<!-- <div class="container-fluid">   
-    
-      <div class="barra"> 
-              <div class="btn-group dropright">
-                 <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                   Bebidas
-               </button>
-            
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-            </div>
-          </div>
-      
+    <div class="barra">
+    <a href="usuario.php"> <img src="https://openclipart.org/download/247319/abstract-user-flat-3.svg" class="float-right" width="20" height="20" style="margin-top:10px;margin:10px;" > </a>
     </div>
 
+  <!-- </div>
 </div> -->
+</div>
+    
+   <div class="container-fluid" style="background-color:#fff;">
+                        <div class="row">
+                            <div class="col-12"> 
+                                
+                                        
+                                 <!-- <div class="row"> -->
+                                     
+
+                                        <div class="col-10">
+                                        
+                                         <img src="alimento2.jpg"  width="1300" height="300" >
+                                           <img src="logo.png"  class="logo" alt="Responsive image">
+                                            
+                                           <button type="button" class="carrinho btn badge badge-pill badge-light">Meu Carrinho  </button>
+                                           <img src="http://www.youthhorizons.org/wp-content/uploads/2017/02/cart-138x150.png" class="icon" width="80" height="80">
+
+                                         </div>
+                                      
+                                <!-- </div> -->
+                             
+                            </div>
+                        </div> 
+                      </div>
+                      <!-- FECHA CONTEUDO INICAL -->
+
+                      <!-- SAUDAÇÕES DE BEM VINDO AO SITE -->
+     <div class="container-fluid" style="background-color:#333645;">
+    <br> 
+        <h4 class="text-center text-info display-3"> Bem-Vindo ao  </h4>
+        <h1 class="text-center text-info display-4"> Delivery Master  </h1> <br>
+        <p class="lead text-center text-white"> A Melhor Experiencia em Compras !!! </p> <br>
+
+        <div class="col-10 offset-1">
+         <blockquote class="blockquote text-center text-white">
+            <p class="mb-0"> Desde nossa inauguração em 2018, temos o orgulho de atender a população de Ji-Paraná.
+                 Através do Delivery Master, nosso compromisso não se restringe a fornecer produtos e serviços de qualidade,
+                 mas sim fazer o máximo para garantir que nossos clientes fiquem completamente satisfeitos.
+                 Convidamos você a navegar pelo site para conhecer tudo o que temos para oferecer. </p>
+         </blockquote>
+        </div>
+    <br>                
+    </div>
+                        <!-- FECHA SAUDAÇÕES DE BEM VINDO -->
+
+                        <!-- PARTE PRINCIPAL DA INDEX - ESCOLHER O MERCADO -->
+    <div class="container-fluid" style="background-color:#fff;">
+    <br> 
+    <h4 class="text-center text-dark display-4"> Escolha um mercado para realizar suas compras </h4> <br>
+       <div class="row">
+      
+
+            <div class="col-10 offset-1">
+           
+                <div class="container-fluid" style="background-color:#fff;">
+                 
+                    <div class="row">
+                        <div class="col-2">              
+                        
+                        <a href="http://www.irmaosgoncalves.com.br/" target="_blanck"><img class="border border-info rounded-circle" src="http://www.unimake.com.br/images/parceiros/danfeviewoffice/irmaosgoncalves.png" width="150" height="150"></a>
+                        </div>
+
+                        <div class="col-2">
+                        <a href="https://www.atacadao.com.br/" target="_blanck"><img class="border border-info rounded-circle" src="http://fingersites.com.br/cozil-final/283-tm_large_default/atacadao.jpg" width="150" height="150"></a>
+                        </div>
+                    
+                        </div> <br>
+                </div>
+                        
+             </div>
+        </div>  <br>       
+    </div>
+                                <!-- FECHA ESCOLHER MERCADO -->
+
+                                <!-- SOBRE NOSSA EQUIPE TOP DO GOIAS -->
+    <div class="container-fluid" style="background-color:#00a2d5;">
+    <br> 
+       <div class="row">
+            <div class="col-5 offset-1" style="background-color:#fff;">
+
+                <div class="container-fluid"> <br>
+                 <h3 class="text-center text-secondary"> Sobre Nosso Software </h3>
+                 <p class="lead text-justify"> 
+                 Delivery Master sempre esteve e sempre estará aqui para atender à comunidade. 
+                 Acreditamos que é importante oferecer aos nossos clientes produtos de qualidade e um ótimo serviço. 
+                 Desde o início o que define quem somos e o que fazemos é nossa verdadeira paixão pela clientela. 
+                 Navegue pelo site e entre em contato para mais informações sobre o(a) Delivery Master.
+                 </p>
+                <br>
+                </div> 
+                        
+            </div>
+
+            <div class="col-5"  style="background-color:#fff;">
+                <img src="team.png" width="400" heigth="400">
+                <br>
+            </div>
+
+        </div>     <br>      
+    </div>
+                                <!-- FECHA TEMA SOBRE EQUIPE -->
+
+                                <!-- SOBRE NOSSOS SERVIÇOS -->
+    <div class="container-fluid" style="background-color:#fff;">
+    <br> 
+    
+       <div class="row">
+      
+
+            <div class="col-10 offset-1">
+           
+                <div class="container-fluid" style="background-color:#fff;">
+                <h2 class="text-center text-secondary"> Nossos Serviços </h2> <br>
+                    <p class="lead text-center"> Conheça mais sobre nossos serviços </p>
+                </div>
+                        
+             </div>
+        </div>  <br>       
+    </div>
+                                <!-- FECHA CONTEUDO SOBRE SERVIÇOS -->
+
+                                <!-- NOSSO SERVIÇO EM DESTAQUE -->
+      <div class="container-fluid" style="background-color:#333645;">
+    <br> 
+    <div class="col-10 offset-1">
+           
+           
+            
+             <div class="row">
+                   <div class="col-6">   
+                      <img src="https://s3.amazonaws.com/igd-wp-uploads/2016/05/Como-Fazer-uma-Pesquisa-de-Mercado-para-o-seu-Nego%CC%81cio-Online.jpg" width="700" heigth="400">         
+                      <!-- https://ecommercenews.com.br/wp-content/uploads/2016/06/carrinho-de-compra.jpg -->
+                    </div>
+
+             
+                   <div class="col-6" style="background-color:#fff;">   
+                        <h3 class="text-center"> Mercado Online </h3> <br>
+                        <p class="lead text-justify"> Este é um dos nossos serviços mais populares. 
+                        Fez uma grande diferença para muitos de nossos clientes, e é oferecido no mais alto nível de excelência.
+                         Com ele, asseguramos que todos os detalhes sejam simples, dinâmicos e executados em tempo útil. 
+                        Sempre que trabalhar com o(a) Delivery Master, você pode confiar que estará em ótimas mãos. <p> 
+                    </div>       
+            </div> <br>
+
+            <div class="row">
+                   <div class="col-6">   
+                      <img src="http://dconheels.com/wp-content/uploads/2018/09/grocery-deivery-shutterstock.jpg" width="700" heigth="400">         
+                    </div>
+
+             
+                   <div class="col-6" style="background-color:#fff;">   
+                        <p class="lead text-center"> Entregas </p>
+                        <p class="text-justify"> Este é um dos nossos serviços mais populares. 
+                        Fez uma grande diferença para muitos de nossos clientes, e é oferecido no mais alto nível de excelência.
+                         Com ele, asseguramos que todos os detalhes sejam simples, dinâmicos e executados em tempo útil. 
+                        Sempre que trabalhar com o(a) Delivery Master, você pode confiar que estará em ótimas mãos. <p> 
+                    </div>       
+            </div>
 
 
 
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
+        </div>  <br>       
+    </div>
+                            <!-- FECHA NOSSO SERVIÇO -->
+
+    
+
+                    <!-- FORMULARIO DE CONTATO
+
+    <div class="container-fluid" style="background-color:#00a2d5;">
+    <br> 
+       <div class="row">
+            <div class="col-10 offset-1" style="background-color:#fff;">
+            <div class="shadow-lg p-3 mb-5 bg-white rounded">
+            <form>
+                <div class="form-row">
+                    <div class="col-md-4 mb-3">
+                    <label for="validationDefault01">First name</label>
+                    <input style="border:none;border-bottom: 2px solid black;" type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Mark" required>
+                    </div>
+    
+            </form>
+            
+            </div>
+            </div>   
+                        
+        </div>
+
+                      
+    </div> -->
+
+</body>
 </html>
